b67feefd381a072284686b3f99273a3b4a3b7e72
DeepaKovvada <118762067+DeepaKovvada@users.noreply.github.com>
2023-01-22 14:25:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
  "azurerm_resource_group" "example" {
-  name     = "datarg001"
+  name     = "rg001"
   location = "canada central"
 }
 resource "azurerm_public_ip" "example" {
   name                = "pip001"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
 
   tags = {
